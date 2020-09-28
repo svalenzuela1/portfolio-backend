fc3d9f4a621a1c92f4a26bc7c8f0d2a7db577501
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2020-09-27 21:12:26 -0400

    routes set up

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,12 +1,68 @@
 const projectModel = require('../models/schemas.js')
 
 //CRUD
+//GET
 const index = async (req, res) => {
     
     try {
 
-} catch(error){
+        const indexModel = await projectModel.find({})
+        res.status(200).json(indexModel)
+
+    } catch(error){
+
+        res.status(400).send(error)
+
+    }
 
 }
 
+//POST
+const create = async(req, res) => {
+    try {
+        
+        const createModel = await projectModel.create(req.body)
+        const indexModel = await projectModel.find({})
+        res.status(200).json(indexModel)
+
+    } catch(error){
+
+        res.status(400).send(error)
+
+    }
 }
+
+//UPDATE
+const update = async (req, res) => {
+    try{
+
+        const updateModel = await projectModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        const indexModel = await projectModel.find({})
+        res.status(200).json(indexModel)
+
+    }catch(error){
+
+        res.status(400).send(error)
+
+    }
+}
+
+//DELETE
+const destroy = async(req, res) => {
+    try{
+
+        const destroyModel = await projectModel.findByIdAndDelete(req.params.id)
+        const indexModel = await projectModel.find({})
+        res.status(200).json(indexModel)
+
+    }catch(error){
+
+        res.status(400).send(error)
+
+    }
+}
+
+
+module.exports = {index, create, update, destroy}
+
+
